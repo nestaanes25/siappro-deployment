f33ffd6cca1a1012bd5bb11f883f6dc17994f058
anestiandinesta <anestiandi05@gmail.com>
2026-03-15 22:02:50 +0700

    bismillah

--- api/index.php
+++ api/index.php
@@ -1,41 +1,4 @@
 <?php
 
-putenv('APP_DEBUG=true');
-putenv('LOG_CHANNEL=stderr');
-putenv('CACHE_STORE=array');
-putenv('SESSION_DRIVER=cookie');
-
-// Create storage directories in /tmp for Vercel read-only filesystem
-$storage = '/tmp/storage';
-$dirs = [
-    "$storage/framework/views",
-    "$storage/framework/cache/data",
-    "$storage/framework/sessions",
-    "$storage/logs",
-    "$storage/bootstrap/cache",
-];
-
-foreach ($dirs as $dir) {
-    if (!is_dir($dir)) {
-        mkdir($dir, 0777, true);
-    }
-}
-
-// Redirect storage path for Laravel
-$_ENV['VERCEL_VIRTUAL_STORAGE'] = $storage;
-
-try {
-    require __DIR__ . '/../public/index.php';
-} catch (\Throwable $e) {
-    // Log error to stderr for Vercel
-    $msg = sprintf(
-        "Fatal Error: %s in %s:%d\nStack trace:\n%s",
-        $e->getMessage(),
-        $e->getFile(),
-        $e->getLine(),
-        $e->getTraceAsString()
-    );
-    error_log($msg);
-    echo "<h1>A fatal error occurred</h1>";
-    echo "<pre>" . htmlspecialchars($msg) . "</pre>";
-}
+// Forward Vercel requests to normal index.php
+require __DIR__ . '/../public/index.php';
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$app = Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
@@ -18,10 +18,3 @@ $app = Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
-
-if (isset($_ENV['VERCEL_VIRTUAL_STORAGE'])) {
-    $app->useStoragePath($_ENV['VERCEL_VIRTUAL_STORAGE']);
-    $app->useBootstrapPath($_ENV['VERCEL_VIRTUAL_STORAGE'] . '/bootstrap');
-}
-
-return $app;
